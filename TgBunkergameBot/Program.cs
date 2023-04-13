77e81f30844691468c8ffe4cc7a0d44ba48bd612
Oleg <super.vec2013@gmail.com>
2023-04-13 21:32:46 +0300

    Добавьте файлы проекта.

---
+++ TgBunkergameBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using CancellationTokenSource cts = new CancellationTokenSource();
+
+string token = "";
+TelegramBotClient client = new TelegramBotClient(token);
+
+ReceiverOptions receiver = new ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+client.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorsAsync,
+    receiverOptions: receiver,
+    cancellationToken: cts.Token
+    );
+async Task HandlePollingErrorsAsync(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+{
+    throw new NotImplementedException();
+}
+
+async Task HandleUpdateAsync(ITelegramBotClient arg1, Update arg2, CancellationToken arg3)
+{
+    throw new NotImplementedException();
+}
